a469876cbda059c0a5b4090567a57ff5ea41e9a6
Denzel <dbartolaba123@gmail.com>
2022-05-03 15:03:34 -0700

    Added in useEffect for details

--- src/views/CharacterDetail.js
+++ src/views/CharacterDetail.js
@@ -2,6 +2,17 @@ import { useEffect, useState } from "react";
 import { singleCharacter } from "../services/fetch-utils";
 import { Link } from 'react-router-dom';
 export default function CharacterDetail() {
+  const [character, setCharacters] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getCharacter = async () => {
+      const details = await singleCharacter();
+      setCharacters(details);
+      setLoading(false);
+    };
+    getCharacter();
+  }, [])
   return (
     <div>CharacterDetail</div>
   )
