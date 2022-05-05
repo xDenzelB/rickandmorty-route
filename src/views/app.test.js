1ec84e9ae198394c96dc2018829eba2b980d2ed0
Denzel <dbartolaba123@gmail.com>
2022-05-05 09:33:03 -0700

    Added in second test

--- src/views/app.test.js
+++ src/views/app.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event'
 import App from '../App';
@@ -6,7 +6,7 @@ import App from '../App';
 describe('App', () => {
   it('Should navigate from list and detail views', async () => {
     render(
-      <MemoryRouter initialEntries={['/characters']}>
+      <MemoryRouter>
         <App />
       </MemoryRouter>
     );
@@ -18,5 +18,28 @@ describe('App', () => {
 
     await screen.findByText('Loading character...');
     await screen.findByText('Rick Sanchez');
+  })
+
+  it('Should render the details page', async () => {
+    render(
+      <MemoryRouter initialEntries={['/characters']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    screen.getByText(/loading/i);
+    const link = await screen.findByText('Morty Smith');
+    userEvent.click(link);
+
+    await waitForElementToBeRemoved(screen.getByText(/loading character.../i));
+    await screen.findByText('Character Details');
+    await screen.findByText('Morty Smith');
+    await screen.findByText('Species: Human');
+    await screen.findByText('Status: Alive');
+    await screen.findAllByAltText('Image');
+    const backLink = await screen.findByText('Back to List');
+    userEvent.click(backLink);
+
+
   })
 })
